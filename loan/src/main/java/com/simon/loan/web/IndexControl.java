b86c55d46cfcbc5bc2bcb302e2b132994a8361b9
Simon <Simon@Simon-PC>
2016-12-27 21:58:14 +0800

    初始化

--- loan/src/main/java/com/simon/loan/web/IndexControl.java
+++ loan/src/main/java/com/simon/loan/web/IndexControl.java
@@ -1,13 +1,22 @@
 package com.simon.loan.web;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.simon.loan.service.ILoanService;
 
 @RestController  
 @RequestMapping("/index")
 public class IndexControl {
 
+	
+	@Resource
+	private ILoanService service;
+	
+	
 	@RequestMapping("/{id}")  
     public String list(@PathVariable("id") Long id) {  
 		System.out.println("----:"+id);
@@ -16,6 +25,7 @@ public class IndexControl {
 	
 	@RequestMapping("/ii")
     public String helloWorld(){
+		service.getLoanList();
         return "hel";
     }
 }
